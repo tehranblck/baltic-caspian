d43893f8757dd6abf3693e251a58a2131856c580
Tehran Bayramov <nicosspyros@gmail.com>
2025-03-06 18:35:09 +0400

    dsf

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['api.balticcaspian.com'],
+        domains: ['api.balticcaspian.com', 'balticcaspian.com'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'api.balticcaspian.com',
+                port: '',
+            },
+        ],
     },
 }
 
